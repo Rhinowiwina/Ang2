aaf922555b67a9b9f4cdeeced59816f16e935927
rwoodall1 <randy@305spin.com>
2017-05-23 06:58:16 -0500

    commit

---
+++ src/LS.WebApp/Controllers/MVC/AngularAccessController-2017-05-22_22-01-20-032.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+//using Webapp.CustomAttributes;
+//using Microsoft.AspNet.Identity.Owin;
+using LS.WebApp;
+
+namespace LS.WebApp.Controllers.mvc
+{
+    //[AuthorizeRedirect("/login")]
+    public class AngularAccessController : Controller
+    {
+        //private ApplicationSignInManager _signInManager;
+        //public ApplicationSignInManager SignInManager
+        //{
+        //    get
+        //    {
+        //        return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+        //    }
+        //    private set
+        //    {
+        //        _signInManager = value;
+        //    }
+        //}
+
+        public async Task<ActionResult> Index()
+        {
+            // TODO: When the time is right, remove this you must, young padawan
+//            await SignInManager.PasswordSignInAsync("Administrator", "Test3r123!", true, false);
+
+            return File("~/app/index.html", "text/html");
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/LS.WebApp/app/app.component-2017-05-22_21-59-56-517.js
@@ -0,0 +1,39 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("@angular/core");
+var branding_service_1 = require("./Service/branding.service");
+var AppComponent = (function () {
+    function AppComponent(_brandingService) {
+        this._brandingService = _brandingService;
+    }
+    AppComponent.prototype.ngOnInit = function () {
+        this.GetBranding();
+    };
+    AppComponent.prototype.GetBranding = function () {
+        var _this = this;
+        this._brandingService.get("api/company/getCompany?companyId=65eab0c7-c7b8-496b-9325-dd8c9ba8ce1c")
+            .subscribe(function (branding) {
+            _this.branding = branding.Data;
+            return _this.branding;
+        }, function (error) { return _this.msg = error; });
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: "my-app",
+        template: "\t\n\t\t\t\t<app-header [brandingmodel]=\"branding\"> \n\t\t\t\t</app-header>\n\t\t\t\t<div class='container'>\n\t\t\t\t<router-outlet></router-outlet>\n\t\t\t\t</div>\n\t\t\t",
+        providers: [branding_service_1.BrandingService],
+    }),
+    __metadata("design:paramtypes", [branding_service_1.BrandingService])
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
